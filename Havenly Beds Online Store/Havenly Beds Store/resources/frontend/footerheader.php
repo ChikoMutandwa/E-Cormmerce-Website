abb1660900086a931df55cad37ba1c115578aa20
Chiko Mutandwa <40405823+ChikoMutandwa@users.noreply.github.com>
2020-08-27 20:59:24 +0200

    Add files via upload

---
+++ Havenly Beds Online Store/Havenly Beds Store/resources/frontend/footerheader.php
@@ -0,0 +1,72 @@
+<!-- /Main Section   -->
+<footer>
+        <div class="container-fluid px-5">
+            <div class="row">
+                <div class="col-md-4 col-sm-12">
+                    <h4>Contact Us</h4>
+                    <div class="row pl-md-1 text-secondary">
+                        <div class="col-12">
+                            <i class="fa fa-home px-md-2"></i>
+                            <small>45 Woodbery Road Southern Surbs WC 7823 South Africa</small>
+                        </div>
+                    </div>
+                    <div class="row pl-md-1 text-secondary py-4">
+                        <div class="col-12">
+                            <i class="fa fa-paper-plane px-md-2"></i>
+                            <small>HavenlyBeds@gmail.com</small>
+                        </div>
+                    </div>
+                    <div class="row pl-md-1 text-secondary">
+                        <div class="col-12">
+                            <i class="fa fa-phone-volume px-md-2"></i>
+                            <small>+27 (71) 760-1825</small>
+                        </div>
+                    </div>
+
+                    <div class="row social text-secondary">
+                        <div class="col-12 py-3">
+                            <i class="fab fa-twitter"></i>
+                            <i class="fab fa-facebook-f"></i>
+                            <i class="fab fa-google-plus-g"></i>
+                            <i class="fab fa-skype"></i>
+                            <i class="fab fa-pinterest-p"></i>
+                            <i class="fab fa-youtube"></i>
+                            <i class="fab fa-linkedin-in"></i>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-2 col-sm-12">
+                    <h4>Our Services</h4>
+                    <div class="d-flex flex-column pl-md-3">
+                        <small class="pt-0">Cape Town</small>
+                        <small>Kwazulu Natal</small>
+                        <small>Port Elizabeth</small>
+                        <small>Johannesburg</small>
+                        <small>Pretoria</small>
+                    </div>
+                </div>
+                <div class="col-md-2 col-sm-12">
+                    <h4>Extras</h4>
+                    <div class="d-flex flex-column pl-md-3">
+                        <small class="pt-0">About Store</small>
+                        <small>New Collection</small>
+                        <small>Contact Us</small>
+                        <small>Latest News</small>
+                        <small>Our Sitemap</small>
+                    </div>
+                </div>
+                <div class="col-md-4 follow-us col-sm-12">
+                    <h4>Follow Instagram</h4>
+                    <div class="d-flex flex-row">
+                        <img src="./images/bed1.jpg" alt="Instagram 1" class="img-fluid">
+                        <img src="./images/bed6.jpg" alt="Instagram 2" class="img-fluid">
+                        <img src="./images/bed2.jpg" alt="Instagram 3" class="img-fluid">
+                    </div>
+                    <div class="d-flex flex-row">
+                        <img src="./images/bed3.jpg" alt="Instagram 1" class="img-fluid">
+                        <img src="./images/bed5.jpg" alt="Instagram 2" class="img-fluid">
+                        <img src="./images/bed4.jpg" alt="Instagram 3" class="img-fluid">
+                    </div>
+                </div>
+            </div>
+        </div>
